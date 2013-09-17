764e23fedee92f9201a53a6b85d85d4a16ac60b3
Thomas <thomas@PC-de-Thomas>
2013-09-17 17:32:06 +0200

    Version où fonctionne à peu près - les automates : * Dans le cas de sous automates, si le sous automate n'est pas ajouté en dernier grace à addState, il n'est pas traduit : BUG à vérifier. * La transition d'état finale n'est toujours pas implémentée
    - la fermeture de Kleene :
    Pas vraiment de bug remonté
    
    - la qSynch :
    La synchronisation ne marche pas mais la version interleaving a l'air de plus où moins marcher (sauf dans le cas de l'étoile de Kleene qui semble faire apparaître une looping recursion...)

--- b.ml
+++ b.ml
@@ -16,14 +16,17 @@ type relation =
 |Lambda of string * expression * string
 |OverLoad of relation * relation;;
 
+type affectation =
+|VarAffect of string * string list * string
+|RelAffect of relation * relation;;
+
 (*Définition de la substitution :
 -Soit c'est une affectation : on affecte à la variable (string1), paramétrée (string list) une valeur (string2)
 -Soit c'est un Select dont la condition est l'expression et dont le then est une nouvelle substitution
 *)
 
 type substitution =
-|Affectation of (string * (string list) * string) list
-|RelationAffectation of (relation * relation)
+|AffectationSub of affectation list
 |Select of (expression * substitution) list;;
 
 (*Définition d'une opération qui a
@@ -116,14 +119,14 @@ let rec printParams param = match param with
 (*Permet d'imprimer le type Expression*)
 
 let rec printExpr n expr = match expr with
-  |And (p1,p2) -> printExpr n p1 ^ " &\n" ^ printExpr n p2
-  |Or (p1,p2) -> "(" ^ printExpr n p1 ^ ") or\n(" ^ printExpr n p2 ^ ")"
+  |And (p1,p2) -> "(" ^ printExpr n p1 ^ " &\n" ^  printExpr n p2 ^ ")"
+  |Or (p1,p2) ->  "(" ^ printExpr n p1 ^  " or\n"  ^ printExpr n p2 ^ ")"
   |Guard g -> printTab n ^ g
   |Comparison (var,params,value) -> (match params with
     |[] -> printTab n ^ var ^ " = " ^ value
     |h::t -> printTab n ^ var ^ "(" ^ printParams params ^ ") = " ^ value)
   |Exists (vars,expression) -> printTab n ^ "#(" ^ printParams vars ^ ").\n(" ^ printExpr n expression ^ ")"
-  |ForAll (vars,expr1,expr2) -> printTab n ^ "!(" ^ printParams vars ^ ").\n(" ^ printExpr n expr1 ^ " =>\n" ^ printExpr n expr2 ^ ")"
+  |ForAll (vars,expr1,expr2) -> printTab n ^ "!(" ^ printParams vars ^ ").\n" ^ printTab n ^ "(" ^ printExpr n expr1^ " =>\n" ^ printExpr n expr2 ^ ")"
   |In (var,set) -> var ^ " : " ^ set
 
 let rec printRelation rel = match rel with
@@ -135,24 +138,26 @@ let rec printRelation rel = match rel with
 
 let printPre pre = printExpr 2 pre;;
 
+let rec printAffectation affect n = match affect with
+  |VarAffect (var,param,value) -> (match param with
+    |[] -> printTab n ^ var ^ " := " ^ value
+    |_ -> printTab n ^ var ^ "(" ^ printParams param ^ ") := " ^ value)
+  |RelAffect (rel1,rel2) -> printTab n ^ printRelation rel1 ^ " := " ^ printRelation rel2;;
+
 (*Permet d'afficher une affectation sous la forme v1 := s1 ou v1(tt):=s1*)
 
 let rec printAffectationList n l = match l with
   |[] -> ""
-  |[var,params,value] ->  (match params with
-    |[] -> printTab n ^ var ^ " := " ^ value
-    |h::t -> printTab n ^ var ^ "(" ^ printParams params ^ ") := " ^ value) ^ "\n"
-  |(var,params,value)::t -> (match params with
-    |[] -> printTab n ^ var ^ " := " ^ value
-    |h::t -> printTab n ^ var ^ "(" ^ printParams params ^ ") := " ^ value) ^ "||\n" ^ printAffectationList n t;;
+  |[t] -> printAffectation t n ^ "\n"
+  |head::tail -> printAffectation head n ^ " ||\n" ^ printAffectationList n tail;;
 
 let rec printSelect n clause = match clause with
   |[] -> ""
   |(cond,action)::tail -> printTab n ^ "WHEN\n" ^ printExpr (n+1) cond ^ "\n" ^ printTab n ^ "THEN\n" ^ printThen (n+1) action ^ printSelect n tail
 and printThen n thenn = match thenn with
-  |Affectation l -> printAffectationList n l
-  |Select ((cond,action)::tail) -> printTab n ^ "SELECT\n" ^ printExpr (n+1) cond ^ "\n" ^ printTab n ^ "THEN\n" ^ printThen (n+1) action ^ printSelect n tail ^ printTab n ^ "END\n"
-  |RelationAffectation (rel1,rel2) -> printRelation rel1 ^ " := " ^ printRelation rel2;;
+  |AffectationSub l -> printAffectationList n l
+  |Select ((cond,action)::tail) -> printTab n ^ "SELECT\n" ^ printExpr (n+1) cond ^ "\n" ^ printTab n ^ "THEN\n" ^ printThen (n+1) action ^ printSelect n tail ^ printTab n ^ "END\n";;
+
 
 (*Affiche une opération simple*)
 
--- trad.ml
+++ trad.ml
@@ -1,159 +1,19 @@
-(*exception AddOpNonValide;;
+#use "astd.ml"
+#use "b.ml"
 
-let rec getStateList astd = match astd with
-  |Elem e -> [Elem e]
-  |Automaton (_,stateL,_,_,_,_) -> stateL
-  |QInter (_,_,_,sAstd) -> getStateList sAstd;;
-
-let rec addEltSetList e name setsList = match setsList with
-  |[] -> ("AutState_" ^ name,[e])::setsList
-  |(n,l)::t -> if n = "AutState_" ^ name then (n,(e::l))::t else ((n,l)::(addEltSetList e name t));; 
-
-let rec getSets_aux stateList name setsList = match stateList with
-  |[] -> setsList
-  |h::t -> (match h with
-    |Elem e -> addEltSetList e name (getSets_aux t name setsList)
-    |Automaton (n,stateL,_,_,_,_) ->
-      getSets_aux stateL n (addEltSetList n name (getSets_aux t name setsList)));;
-
-let getSets stateList name = getSets_aux stateList name [];;
-
-let rec addVarToVarList name varList = match varList with
-  |[] -> ["State_" ^ name]
-  |h::t -> if h = "State_" ^ name then h::t else h::(addVarToVarList name t);;
-
-let rec getVariables_aux stateList name varList=  match stateList with
-  |[] -> varList
-  |h::t -> (match h with
-    |Elem e -> addVarToVarList name varList
-    |Automaton (n,stateL,_,_,_,_) -> getVariables_aux stateL n (addVarToVarList name (getVariables_aux t name varList)))
-
-let getVariables stateList name = getVariables_aux stateList name [];;
-
-let rec addInvToInvList name invList = match invList with
-  |[] -> ("State_" ^ name,[],"AutState_" ^ name)::invList
-  |(n,l,t)::tail -> if n = "State_" ^ name then (n,l,t)::tail else (n,l,t)::(addInvToInvList name tail);;
-
-let rec getInvariants_aux stateList name invList = match stateList with
-  |[] -> invList
-  |h::t -> (match h with
-    |Elem e -> addInvToInvList name invList
-    |Automaton (n,stateL,_,_,_,_) -> getInvariants_aux stateL n (addInvToInvList name (getInvariants_aux t name invList)));;
-
-let getInvariants stateList name = getInvariants_aux stateList name [];;
-
-let rec addInitToInitList name initVal initList = match initList with
-  |[] -> ("State_" ^ name,[],initVal)::initList
-  |(n,l,v)::tail -> if n = "State_" ^ name then (n,l,v)::tail else (n,l,v)::(addInitToInitList name initVal tail);;
-
-let rec getInit_aux stateList name initList = match stateList with
-  |[] -> initList
-  |h::t -> (match h with
-    |Elem e -> initList
-    |Automaton (n,stateL,_,_,_,init) -> getInit_aux stateL n (addInitToInitList n (getNameOf init) (getInit_aux t name initList)));;
-
-let getInit stateList name = getInit_aux stateList name [];;
-
-(*let rec addOpToOpList opList name pre post gu fi = match opList with
-  |[] -> if gu = "" 
-    then [(name,[],pre,Select [pre,post])] 
-    else [(name,[],And (pre,Guard gu),Select [And (pre,Guard gu),post])]
-  |(na,p,pr,subs)::t -> if name = na then
-      if gu = "" then
-	(na,p,(Or (pre,pr)),(Select ((pre,post)::subs)))::t
-      else
-	(na,p,(Or (And (pre,Guard gu),pr)),(Select ((And (pre,Guard gu), post)::subs)))::t
-    else (na,p,pr,subs)::(addOpToOpList t name pre post gu fi);;
-*)
-
-let rec addOpToOpList opList name pre post gu fi =
-  let preco = if gu = "" then pre else And (pre, Guard gu) in
-  match opList with
-  |[] -> [(name,[],preco,Select [(preco,post)])]
-  |(na,p,pr,subs)::t -> if (name = na) then
-      begin
-	match subs with
-	|Affectation [_,_,_] -> raise AddOpNonValide
-	|Select l -> (na,p,Or (preco,pr),Select ((preco,post)::l))::t
-      end
-    else
-      (na,p,pr,subs)::(addOpToOpList t name pre post gu fi);;
-
-let rec getOperationsTransList_aux transitionList astd opList = match transitionList with
-  |[] -> opList
-  |h::t -> (match h with
-    |Loc (name,sfrom,sto,gu,fi) ->
-      getOperationsTransList_aux t astd
-	(addOpToOpList opList 
-	   name 
-	   (Comparison ("State_" ^ (getNameOfAstdContState sfrom astd),[],sfrom)) 
-	   (Affectation [("State_" ^ (getNameOfAstdContState sto astd),[],sto)])
-	   gu
-	   fi)
-    |Tsub (name,sfrom,stoG,stoS,gu,fi) -> 
-      getOperationsTransList_aux t astd
-	(addOpToOpList 
-	   opList
-	   name 
-	   (Comparison ("State_" ^ (getNameOfAstdContState sfrom astd),[],sfrom))
-	   (Affectation [("State_" ^ (getNameOfAstdContState stoG astd),[],stoG);(stoG,[],stoS)])
-	   gu
-	   fi)
-    |Tfrom (name,sfromG,sfromS,sto,gu,fi) ->
-      getOperationsTransList_aux t astd
-	(addOpToOpList
-	   opList
-	   name
-	   (And ((Comparison ("State_" ^ (getNameOfAstdContState sfromG astd),[],sfromG)),
-		 (Comparison ("State_" ^ sfromG,[],sfromS))))
-	   (Affectation [("State_" ^ (getNameOfAstdContState sto astd),[],sto)])
-	   gu
-	   fi));;
-
-let rec addParamInv invariants var = [];;
-
-let rec addParamInit init var = [];;
-
-let rec addParamOpes operations var = [];;
-
-let addParameter var  typeVar machine = {machine = machine.machine;
-				sets = (typeVar,[])::machine.sets;
-				variables = machine.variables;
-				invariants = addParamInv (machine.invariants) var;
-				initialisation = addParamInit (machine.initialisation) var;
-				operations = addParamOpes machine.operations var};;
-
-let rec getOperationsStateList stateList astd astdG opList = match stateList with
-  |[] -> opList
-  |h::t -> getOperationsStateList t astd astdG (getOperations_aux h opList astdG)
-and getOperations_aux astd opList astdG =match astd with
-  |Elem e -> opList
-  |Automaton (name,stateList,transitionList,sfList,dfList,initialState) ->
-    getOperationsStateList stateList astd astdG (getOperationsTransList_aux transitionList astdG opList);;
-
-
-
-let getOperations astd = getOperations_aux astd [] astd;;
-
-let rec traduction astd = match astd with
-  |Elem e -> {machine = e;
-	      sets = [];
-	      variables = [];
-	      invariants = [];
-	      initialisation = [];
-	      operations = []}
-  |Automaton (name,stateList,transitionList,sfList,dfList,initialState) ->
-    {machine = name;
-     sets = getSets stateList name;
-     variables = getVariables stateList name;
-     invariants = getInvariants stateList name;
-     initialisation = addInitToInitList name (getNameOf initialState) (getInit stateList name);
-     operations = getOperations astd}
-  |QSynch (name,var,typeVar,sAstd) -> addParameter var typeVar (traduction sAstd);;
-*)
 exception TheAstdIsNotWellFounded;;
 exception AddParamToNonExistentVariable;;
+exception NoFinalState;;
 
+(*
+Fonction addSetToSetList
+Objectif :
+Ajouter des éléments pour la clause SET de la machine. Elle ajoute l'élément <name> à l'énumération de l'ensemble AutState_<nameAstdSup>
+Arguments :
+- nameAstdSup : Nom de l'automate dont on rempli l'ensemble
+- name : Nom de l'état de l'automate qu'on souhaite ajouter à l'ensemble
+- setsList : Liste de la forme (string1 * string2 list) list qui liste chaque ensemble (string1) et les différentes valeurs énumérées de cet ensemble (string2 list)
+*)
 
 let rec addSetToSetList nameAstdSup name setsList =
   if name = nameAstdSup then setsList else
@@ -164,6 +24,20 @@ let rec addSetToSetList nameAstdSup name setsList =
       then (nameType,(name::values))::tail
       else (nameType,values)::(addSetToSetList nameAstdSup name tail);;
 
+(*
+Fonction addVarToVarList :
+Objectif :
+Construire la liste des variables nécessaire aux clauses VARIABLES, INVARIANT et INITIALISATION de la machine B. Elle ajoute si elle n'y est pas déjà la variable name à la liste de variable <varList> en lui donnant la valeur <initValue>.
+Arguments :
+- name : nom de la variable
+- varList : Liste de la forme (string1 * string2 list * string3 * string4) qui représentent :
+  - string1 : Le nom de la variable
+  - string2 list : les éventuels paramètres de la variable (dans le cas des astd quantifiés)
+  - string3 : Le type à donner à la variable
+  - string4 : La valeur initiale de cette variable
+- initValue : La valeur initiale à donner à la variable
+*)
+
 let rec addVarToVarList name varList initValue = match varList with
   |[] -> [("State_" ^ name,[],"AutState_" ^ name,initValue)]
   |(varName,params,setType,initValueVar)::tail -> 
@@ -172,6 +46,17 @@ let rec addVarToVarList name varList initValue = match varList with
       varList
     else
       (varName,params,setType,initValueVar)::(addVarToVarList name tail initValue);;
+
+(*
+Fonction addOpToOpList_aux :
+Objectif : Fonction auxiliaire de addOpToOpList. Elle permet d'ajouter à la liste des opérations <opList> déjà constituée l'opération de nom <name>, de paramètres <param>, donc la précondition est <pre> et la substitution est <post>
+Arguments :
+- name : nom de l'opération (obtenu grace au nom de la transition)
+- param : éventuels paramètres de l'opération (obtenus dans la transition)
+- pre : précondition de la forme d'une expression (cf b.ml)
+- post : postcondition de type sublstitution (cf b.ml)
+- opList : liste des opérations déjà construites
+*)
 
 let rec addOpToOpList_aux name param pre post opList = match opList with
   |[] -> [(name,param,pre,Select [pre,post])]
@@ -183,6 +68,13 @@ let rec addOpToOpList_aux name param pre post opList = match opList with
     else
       ((nameOp,paramOp,preOp,(Select l))::(addOpToOpList_aux name param pre post tail));;
 
+(*
+Fonction addOpToOpList :
+Objectif : Ajouter à la liste des opérations <opList> l'opération correspondant à la transition <trans> dans l'ASTD de nom <nameAstd>. Elle construit également les éléments pour la construction des arguments <pre> et <post> de la fonction addOpToOpList
+Arguments :
+- nameAstd : nom de l'ASTD dans lequel se situe l'opération
+- trans : la transition issue de la liste des transition de l'ASTD
+*)
 
 let addOpToOpList nameAstd trans opList = match trans with
   |Loc (name,param,from,toS,gu,finale) -> 
@@ -191,7 +83,7 @@ let addOpToOpList nameAstd trans opList = match trans with
 	Comparison ("State_" ^ nameAstd,[],from) 
       else
 	And (Guard gu,Comparison("State_" ^ nameAstd,[],from))) in
-    let post = Affectation [("State_" ^ nameAstd,[],toS)] in
+    let post = AffectationSub [VarAffect ("State_" ^ nameAstd,[],toS)] in
     addOpToOpList_aux name param pre post opList
   |Tsub (name,param,from,toSup,toState,gu,finale) ->
     let pre = (if gu = ""
@@ -200,7 +92,7 @@ let addOpToOpList nameAstd trans opList = match trans with
       else
 	And (Guard gu,Comparison ("State_" ^ nameAstd,[],from)))
     in
-    let post = Affectation [("State_" ^ nameAstd,[],toSup);("State_" ^ toSup,[],toState)]
+    let post = AffectationSub [VarAffect ("State_" ^ nameAstd,[],toSup);VarAffect ("State_" ^ toSup,[],toState)]
     in
     addOpToOpList_aux name param pre post opList
   |Fsub (name,param,fromSup,fromState,toS,gu,finale) ->
@@ -210,12 +102,27 @@ let addOpToOpList nameAstd trans opList = match trans with
       else
 	And (Guard gu,And (Comparison ("State_" ^ nameAstd,[],fromSup),(Comparison ("State_" ^ fromSup,[],fromState)))))
     in
-    let post = Affectation ["State_" ^ nameAstd,[],toS]
+    let post = AffectationSub [VarAffect ("State_" ^ nameAstd,[],toS)]
     in addOpToOpList_aux name param pre post opList;;
+
+(*
+Fonction getOperationList
+Objectif :
+Permet de récupérer à partir de la liste des transitions <transList> la liste des opérations <opList>.
+Argument :
+- nameAstd : Le nom de l'ASTD dans lequel on veut récupérer les transitions
+- transList : la liste des transition de cet ASTD
+- opList : La liste de opérations qu'on souhaite construire
+*)
 
 let rec getOperationList nameAstd transList opList = match transList with
   |[] -> opList
   |trans::tail -> getOperationList nameAstd tail (addOpToOpList nameAstd trans opList);;
+
+(*
+Fonction addParamToVarList :
+
+*)
 
 let rec addParamToVarList var typeVar nameAstd varList = match varList with
   |[] -> raise AddParamToNonExistentVariable
@@ -227,7 +134,21 @@ let rec isInStringList name strList = match strList with
   |[] -> false
   |head::tail -> head = name or isInStringList name tail;;
 
-let rec modifyQSynchOperation op = op;;
+
+(*let rec modifyAffectation l expr result = match l with
+  |[] -> []
+  |VarAffect(->
+
+let rec modifyPost post expr= match post with
+  |Select l -> modifySelect l
+  |AffectationSub l -> modifyAffectation l expr
+and modifySelect l = match l with
+  |[] -> Select []
+  |[pre,post] -> modifyPost post pre
+  |(pre,post)::tail -> (modifyPost post pre)
+*)
+
+let rec modifyPost post = post;;
 
 let rec addParamToExpr exp var paramVar = match exp with
   |And (expr1,expr2) -> And ((addParamToExpr expr1 var paramVar),(addParamToExpr expr2 var paramVar))
@@ -235,20 +156,45 @@ let rec addParamToExpr exp var paramVar = match exp with
   |Guard s -> Guard s
   |Comparison (name,params,value) -> if name = paramVar then Comparison (name,(var::params),value) else Comparison (name,params,value);;
 
+let rec modifyQSynchOperation op var typeVar paramVar =
+  let name,params,pre,post = op in
+  name,params,ForAll ([var],In (var,typeVar), addParamToExpr pre var paramVar),modifyPost post;;
+
 let rec addParamToAffectationList affList var paramVar = match affList with
   |[] -> raise AddParamToNonExistentVariable
-  |(name,params,value)::tail -> if name = paramVar then (name,(var::params),value)::tail else (name,params,value)::(addParamToAffectationList affList var paramVar);;
+  |(VarAffect (name,params,value))::tail -> if name = paramVar then (VarAffect (name,(var::params),value))::tail else (VarAffect (name,params,value))::(addParamToAffectationList affList var paramVar)
+  |(RelAffect (rel1,rel2))::tail -> (RelAffect (rel1,rel2))::(addParamToAffectationList tail var paramVar);;
 
 let rec addParamToSubstitution sub var paramVar = match sub with
-  |Affectation l -> Affectation (addParamToAffectationList l var paramVar)
+  |AffectationSub l -> AffectationSub (addParamToAffectationList l var paramVar)
   |Select l -> Select (addParamToSelectList l var paramVar)
 and addParamToSelectList list var paramVar = match list with
   |[] -> []
   |(expr,subs)::tail -> (addParamToExpr expr var paramVar, addParamToSubstitution subs var paramVar)::addParamToSelectList tail var paramVar;;
 
-let rec addSynchToOp var delta nameOfAstd opeList = match opeList with
+let rec addSynchToOp var delta nameOfAstd typeVar opeList = match opeList with
   |[] -> []
-  |(name,param,pre,post)::tail -> if isInStringList name delta then (modifyQSynchOperation (name,param,pre,post))::addSynchToOp var delta nameOfAstd tail else (name,param,addParamToExpr pre var ("State_" ^ nameOfAstd),addParamToSubstitution post var ("State_" ^ nameOfAstd))::(addSynchToOp var delta nameOfAstd tail);;
+  |(name,param,pre,post)::tail ->
+    if isInStringList name delta
+    then
+      (modifyQSynchOperation
+	 (name,param,pre,post)
+	 var
+	 typeVar
+	 ("State_" ^ nameOfAstd))::
+	addSynchToOp var delta nameOfAstd typeVar tail 
+    else
+      (name,
+       param,
+       addParamToExpr
+	 pre
+	 var
+	 ("State_" ^ nameOfAstd),
+       addParamToSubstitution
+	 post
+	 var
+	 ("State_" ^ nameOfAstd))::
+	(addSynchToOp var delta nameOfAstd typeVar tail);;
 
 let rec addNonEnumerateSet set setList = match setList with
   |[] -> [set,[]]
@@ -258,7 +204,15 @@ let addKleeneSet setList = ("KleeneState",["started";"notstarted"])::setList;;
 
 let addKleeneVar varList name = ("StateKleene_" ^ name,[],"KleeneState","notstarted")::varList;;
 
-let rec final astd = Guard "L'ASTD est final";;
+let rec final astd name = match astd with
+  |Elem s -> Comparison (("State_" ^ name),[],s)
+  |Automaton (nameAstd,_,_,fState,dFState,_) -> match (fState,dFState) with
+    |[],listDFState -> getFinalFromFinalStateList listDFState nameAstd
+    |listFState, [] -> getFinalFromFinalStateList listFState nameAstd
+    |fState,dFState -> And (getFinalFromFinalStateList fState nameAstd,getFinalFromFinalStateList dFState nameAstd)
+and getFinalFromFinalStateList fStateList name = match fStateList with
+  |[fState] -> final fState name
+  |head::tail -> And (final head name,getFinalFromFinalStateList tail name);;
 
 let rec initOf exp nameOfSAstd initStateVal = match exp with
   |And (exp1,exp2) -> And (initOf exp1 nameOfSAstd initStateVal,initOf exp2 nameOfSAstd initStateVal)
@@ -266,13 +220,13 @@ let rec initOf exp nameOfSAstd initStateVal = match exp with
   |Guard gu -> Guard gu
   |Comparison (name,param,value) -> if name = "State_" ^ nameOfSAstd then Comparison (initStateVal,[],value) else Comparison (name,param,value);;
 
-let rec modifyExprKleene exp astd name nameOfSAstd initStateVal= Or (And (Or (final astd,Comparison ("StateKleen_" ^ name,[],"notstarted")),initOf exp nameOfSAstd initStateVal), exp);;
+let rec modifyExprKleene exp astd name nameOfSAstd initStateVal nameOfAstd = Or (And (Or (final astd nameOfSAstd,Comparison ("StateKleene_" ^ nameOfAstd,[],"notstarted")),initOf exp nameOfSAstd initStateVal), exp);;
 
 let rec modifyPostKleene post = post;;
 
-let rec addKleeneCondition opList astd name nameOfSAstd initStateVal= match opList with
-  |[] -> []
-  |(name,param,pre,post)::tail -> (name,param,modifyExprKleene pre astd name nameOfSAstd initStateVal,modifyPostKleene post)::(addKleeneCondition tail astd name nameOfSAstd initStateVal);;
+let rec addKleeneCondition opList astd nameOfAstd nameOfSAstd initStateVal= match opList with 
+ |[] -> []
+  |(name,param,pre,post)::tail -> (name,param,modifyExprKleene pre astd name nameOfSAstd initStateVal nameOfAstd,modifyPostKleene post)::(addKleeneCondition tail astd name nameOfSAstd initStateVal);;
 
 let rec getInitOf astd = match astd with
   |Elem e -> Elem e
@@ -291,11 +245,11 @@ let rec traduction_aux astd nameAstdSup setsList varList opeList= match astd wit
       (addVarToVarList name varListInd (getNameOf initialState)),
       (getOperationList name transitionList opeListInd))
   |QSynch (name,var,typeVar,delta,sAstd) -> 
-    let (setsListInd,varListInd,opeListInd) = traduction_aux sAstd name setsList varList opeList in
-    (addNonEnumerateSet typeVar setsListInd,addParamToVarList var typeVar (getNameOf sAstd) varListInd,addSynchToOp var delta (getNameOf sAstd) opeListInd)
+    let (setsListInd,varListInd,opeListInd) = traduction_aux sAstd (getNameOf sAstd) setsList varList opeList in
+    (addNonEnumerateSet typeVar setsListInd,addParamToVarList var typeVar (getNameOf sAstd) varListInd,addSynchToOp var delta (getNameOf sAstd) typeVar opeListInd)
   |Kleene (name,sAstd) ->
-    let (setsListInd,varListInd,opeListInd) = traduction_aux sAstd name setsList varList opeList in
-    ((addKleeneSet setsListInd),(addKleeneVar varListInd name),addKleeneCondition opeList sAstd name (getNameOf sAstd) (getInitNameOf sAstd))
+    let (setsListInd,varListInd,opeListInd) = traduction_aux sAstd (getNameOf sAstd) setsList varList opeList in
+    ((addKleeneSet setsListInd),(addKleeneVar varListInd name),addKleeneCondition opeListInd sAstd name (getNameOf sAstd) (getInitNameOf sAstd))
 and traductionStateList stateList nameAstdSup setsList varList opeList = match stateList with
   |[] -> setsList,varList,opeList
   |head::tail -> let (setsListInd,varListInd,opeListInd) =
